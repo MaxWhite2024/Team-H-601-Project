e0a2b80009f96bb3376d9da0c6552e41b5cdad17
MaxWhite2024 <mjwhite@scu.edu>
2024-11-05 20:40:32 -0300

    UI dependency on player disabled

--- Team H Unity Project/Assets/PlayerAmmoUI.cs
+++ Team H Unity Project/Assets/PlayerAmmoUI.cs
@@ -28,8 +28,8 @@ public class PlayerAmmoUI : MonoBehaviour
     {
         p1AmmoText.text = "P1 Ammo: " + playerMovementAndAttack.p1Ammo;
         p2AmmoText.text = "P2 Ammo: " + playerMovementAndAttack.p2Ammo;
-        p1ModeText.text = "P1 Mode: " + PlayerMode.GetName(typeof(PlayerMode), (int)playerMovementAndAttack.p1Mode);
-        p2ModeText.text = "P2 Mode: " + PlayerMode.GetName(typeof(PlayerMode), (int)playerMovementAndAttack.p2Mode);
+        //p1ModeText.text = "P1 Mode: " + PlayerMode.GetName(typeof(PlayerMode), (int)playerMovementAndAttack.p1Mode);
+        //p2ModeText.text = "P2 Mode: " + PlayerMode.GetName(typeof(PlayerMode), (int)playerMovementAndAttack.p2Mode);
         P1AmmoRechargeSlider.value = playerMovementAndAttack.tempP1AmmoRechargeTime;
         P2AmmoRechargeSlider.value = playerMovementAndAttack.tempP2AmmoRechargeTime;
     }
