c760412fdad6158f6968992222b907c3a25f0d29
MaxWhite2024 <mjwhite@scu.edu>
2024-11-29 20:31:25 -0300

    save

--- Team H Unity Project/Assets/Scripts/BurstParticleDestruction.cs
+++ Team H Unity Project/Assets/Scripts/BurstParticleDestruction.cs
@@ -7,6 +7,7 @@ public class BurstParticleDestruction : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //destroy gameObject when burst particle system has finished
         ParticleSystem particleSystem = gameObject.GetComponent<ParticleSystem>();
         var main = particleSystem.main;
         Destroy(gameObject, main.duration);
