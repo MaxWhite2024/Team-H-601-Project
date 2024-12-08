d76c7076d22807042990f2546d76e6c967242fbb
MaxWhite2024 <mjwhite@scu.edu>
2024-12-08 18:31:11 -0300

    canvas fixes

--- Team H Unity Project/Assets/Scripts/CutsceneManager.cs
+++ Team H Unity Project/Assets/Scripts/CutsceneManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
-using UnityEditor.ShaderGraph.Drawing.Inspector.PropertyDrawers;
+//using UnityEditor.ShaderGraph.Drawing.Inspector.PropertyDrawers;
 using System.Linq;
 
 
